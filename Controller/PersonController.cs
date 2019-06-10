d608ff6466aa564b509b31c5ce6e951db4394af3
dcoleman90 <dcolem12@my.westga.edu>
2019-06-09 21:35:24 -0400

    Added Model - Controller - DAL folders
    Person Model - Controller and DAL added
    
    Added initial 'Insert Person' code has not yet been tested
    
    Added ScheduleManager_DB_Connection class which provides method to connect to the database

---
+++ Controller/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using ScheduleManager.Model;
+using ScheduleManager.DAL;
+
+namespace ScheduleManager.Controller
+{
+    /// <summary>
+    /// This class insures proper access to the DAL \
+    /// and controls all manipulation of the person object(s)
+    /// </summary>
+    public class PersonController
+    {
+        PersonDAL personDAL = new PersonDAL();
+        public void AddPerson(Person addedPerson)
+        {
+            this.personDAL.AddPerson(addedPerson);
+        }
+    }
+}
---
+++ DAL/PersonDal.cs
@@ -0,0 +1,43 @@
+﻿using ScheduleManager.Model;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ScheduleManager.DAL
+{
+    /// <summary>
+    /// This class provides access to the database 
+    /// It is conserned with Person 
+    /// </summary>
+    public class PersonDAL
+    {
+        public void AddPerson(Person addPerson)
+        {
+            SqlConnection connection = ScheduleManager_DB_Connection.GetConnection();
+            try
+            {
+                SqlCommand command = new SqlCommand("spAddEmployee", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@last_name", addPerson.LastName));
+                command.Parameters.Add(new SqlParameter("@first_name", addPerson.LastName));
+                command.Parameters.Add(new SqlParameter("@date_of_birth", addPerson.LastName));
+                command.Parameters.Add(new SqlParameter("@ssn", addPerson.LastName));
+                command.Parameters.Add(new SqlParameter("@gender", addPerson.LastName));
+                command.Parameters.Add(new SqlParameter("@street_address", addPerson.LastName));
+                command.Parameters.Add(new SqlParameter("@phone", addPerson.LastName));
+                command.Parameters.Add(new SqlParameter("@zipcode", addPerson.LastName));
+                command.Parameters.Add(new SqlParameter("@username", addPerson.LastName));
+
+                connection.Open();
+                SqlDataReader dataReader = command.ExecuteReader();
+                connection.Close();
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }            
+            }
+        }
+    }
+}
